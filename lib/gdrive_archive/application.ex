2424a6d48e19a986eb6b045051ffe725eb4cf6e6
Guillermo Galan <guille@clutchapp.io>
2024-02-04 20:16:03 +0100

    add application supervisor

---
+++ lib/gdrive_archive/application.ex
@@ -0,0 +1,20 @@
+defmodule GdriveArchive.Application do
+  # See https://hexdocs.pm/elixir/Application.html
+  # for more information on OTP Applications
+  @moduledoc false
+
+  use Application
+
+  @impl true
+  def start(_type, _args) do
+    children = [
+      # Starts a worker by calling: GdriveArchive.Worker.start_link(arg)
+      # {GdriveArchive.Worker, arg}
+    ]
+
+    # See https://hexdocs.pm/elixir/Supervisor.html
+    # for other strategies and supported options
+    opts = [strategy: :one_for_one, name: GdriveArchive.Supervisor]
+    Supervisor.start_link(children, opts)
+  end
+end
