8393c68f7fe6b6500bc113acbb2a3034cf3cc90a
Guillermo Galan <guille@clutchapp.io>
2024-04-02 20:01:14 +0200

    implement function to recursively compute tree sizes

--- lib/gdrive_archive/tree_processor.ex
+++ lib/gdrive_archive/tree_processor.ex
@@ -12,4 +12,32 @@ defmodule GdriveArchive.TreeProcessor do
     end)
   end
 
+  def compute_tree_size(tree) do
+    tree
+    |> Enum.map(fn root -> process_node_size(root) end)
+  end
+
+  defp process_node_size(%{children: []} = node) do
+    node
+    |> Map.put_new(:children_size, 0)
+  end
+
+  defp process_node_size(%{children: children} = node) when is_list(children) do
+    # Recursively process all children of this node
+    children_with_size =
+      children
+      |> Enum.map(&process_node_size/1)
+
+    # Add up all of the sizes of the direct children of this node
+    children_size =
+      Enum.reduce(children_with_size, 0, fn child, acc ->
+        acc + (child.size || 0) + (child.children_size || 0)
+      end)
+
+    node
+    |> Map.put(:children_size, children_size)
+    |> Map.put(:children, children_with_size)
+  end
+
+  defp process_node_size(node), do: node
 end
